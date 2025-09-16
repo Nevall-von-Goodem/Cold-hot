3c47fca0bd3f8eed726d4e0758fd8ffa61db1483
Nevall von Goodem <sokoloff.roman.ru@mail.ru>
2025-09-16 18:00:19 +0300

    Task02

---
+++ Task02/cold-hot/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sokol/cold-hot',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'e272f61f9a02b8b30f8774bf2059d55116d9f850',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sokol/cold-hot' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'e272f61f9a02b8b30f8774bf2059d55116d9f850',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wp-cli/php-cli-tools' => array(
+            'pretty_version' => 'v0.12.6',
+            'version' => '0.12.6.0',
+            'reference' => 'f12b650d3738e471baed6dd47982d53c5c0ab1c3',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../wp-cli/php-cli-tools',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
